2d967065afa398096aad1dec9cb59d3e90020b42
DenisAGS <denissearelyg@gmail.com>
2021-11-16 07:23:48 -0600

    develop

---
+++ src/app/area-calculator/area-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-area-calculator',
+  templateUrl: './area-calculator.component.html',
+  styleUrls: ['./area-calculator.component.css']
+})
+export class AreaCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
